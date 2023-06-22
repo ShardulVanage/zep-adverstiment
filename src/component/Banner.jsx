d14dce4e2e758e10977cb92d6582b99af4eb749e
ShardulVanage <vsharadul@gmail.com>
2023-06-22 23:08:17 +0530

    mobile changes

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -39,17 +39,19 @@ export default function Banner() {
                 >
                     {/* Background shape */}
                 </div>
-                <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-                    <p className="text-sm leading-6 text-gray-900">
-                        <strong className="font-semibold">Data Analytics Masters 2023.</strong>
+                <div className="flex sm:flex-wrap items-center gap-x-4 gap-y-2 justify-center flex-row">
+                    <p className="sm:text-sm sm:leading-6 text-gray-900 text-xs flex sm:flex-row flex-col">
+                        <strong className="font-semibold  ">Data Analytics Masters 2023.</strong>
+                        <hr />
                         <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
                             <circle cx={1} cy={1} r={1} />
                         </svg>
                         Zep Analytics. Join us before {nextMonthDateString} 1 to avail 50% discount
                     </p>
+
                     <a
                         href="https://t.me/+cHd6h0_F3ZA0ZmRl"
-                        className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+                        className="flex-none rounded-full bg-gray-900 px-3.5 py-1 sm:text-sm text-xs font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
                     >
                         Join Us <span aria-hidden="true">&rarr;</span>
                     </a>
